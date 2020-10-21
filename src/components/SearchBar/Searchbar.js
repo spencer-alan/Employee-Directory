438230959a5740120db88d3a581d3e848bee3962
b1acksmyth <smith.spence13@gmail.com>
2020-10-21 15:12:27 -0700

    search bar with filter dropdown and custom css

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -1,13 +1,30 @@
 import React from "react";
+import "./searchbar.css";
 
 
 function Searchbar() {
   return (
     <div className="container">
-      <div className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Search for an Employee" aria-label="Search for an Employee" aria-describedby="basic-addon2" />
-        <div className="input-group-append">
-          <button className="btn btn-outline-info" type="button">Search</button>
+      <div className="row">
+        <div  className="col-8">
+          <div id="search" className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search for an Employee" aria-label="Search for an Employee" aria-describedby="basic-addon2" />
+            <div className="input-group-append">
+              <button className="btn btn-outline-info" type="button">Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 clearfix">
+          <div id="filter" className="form-inline">
+            <select id="deptFilter" className="form-control">
+              <option selected>Filter...</option>
+              <option value="administration">Administration</option>
+              <option value="publicRelations">Public Relations</option>
+              <option value="legal">Legal</option>
+              <option value="infoTech">Information Tech</option>
+            </select>
+            <button type="submit" className="btn btn-outline-info">Search</button>
+          </div>
         </div>
       </div>
     </div>
