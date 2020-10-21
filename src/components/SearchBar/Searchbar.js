267f89ea489b648a0396acafab38a8bae889a81e
b1acksmyth <smith.spence13@gmail.com>
2020-10-21 01:48:01 -0700

    added basic structure of searchbar

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -5,7 +5,7 @@ function Searchbar() {
   return (
     <div className="container">
       <div className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Search for and Employee" aria-label="Search for and Employee" aria-describedby="basic-addon2" />
+        <input type="text" className="form-control" placeholder="Search for an Employee" aria-label="Search for an Employee" aria-describedby="basic-addon2" />
         <div className="input-group-append">
           <button className="btn btn-outline-info" type="button">Search</button>
         </div>
