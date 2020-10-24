e11cbd50067e9b4fa17d20825e1259785d21c7f1
b1acksmyth <smith.spence13@gmail.com>
2020-10-23 20:48:08 -0700

    updated row to work as a component

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Row() {
-  return <div className="row" />
+function Row(props) {
+  return <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
 
 export default Row;
